07feaf9ad2d1652bd6ae167dbfd42a97f4f92ac8
iamiamwater <abcdefg123890123890123890@gmail.com>
2025-12-24 10:23:31 -0500

    Update save.js

--- js/utils/save.js
+++ js/utils/save.js
@@ -227,7 +227,7 @@ function loadOptions() {
 }
 function setupModInfo() {
 	modInfo.changelog = changelog;
-	modInfo.winText = "winText ? winText : `Congratulations! You have reached the end and beaten this game, but for now...";
+	modInfo.winText = "winText ? winText : 'Congratulations! You have reached the end and beaten this game, but for now...";
 
 }
 function fixNaNs() {
